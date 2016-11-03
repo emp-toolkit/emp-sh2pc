d422f85a39b6a59eb8a8c4db2950f181b84b8912
Ubuntu <ubuntu@ip-172-31-6-191.ec2.internal>
2016-11-03 15:38:58 +0000

    use cmake to locate

--- emp-sh2pc-config.cmake
+++ emp-sh2pc-config.cmake
@@ -1,6 +1,6 @@
 find_package(emp-ot)
 
-find_path(EMP-SH2PC_INCLUDE_DIR emp-sh2pc/emp-sh2pc.h)
+find_path(EMP-SH2PC_INCLUDE_DIR cmake/emp-sh2pc-config.cmake)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EMP-SH2PC DEFAULT_MSG EMP-SH2PC_INCLUDE_DIR)
