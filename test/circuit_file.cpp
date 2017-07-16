4529d986e185c8b40d6b46ae23b62d887edc613c
Xiao Wang <wangxiao1254@gmail.com>
2017-07-16 01:08:45 -0400

    remove wallClock

--- test/circuit_file.cpp
+++ test/circuit_file.cpp
@@ -25,26 +25,25 @@ int main(int argc, char** argv) {
 		local_gc = new HalfGateGen<MemIO>(memio);
 	else 
 		local_gc = new HalfGateEva<MemIO>(memio);
-	if(party == ALICE) {		
-		double t1 = wallClock();
+	if(party == ALICE) {	
+		auto start = clock_start();
 		for(int i = 0; i < 10000; ++i) {
 			memio->clear();
 			cf.compute((block*)c.bits, (block*)a.bits, (block*)b.bits);
 		}
-		cout << wallClock() - t1<< endl;
+		cout << time_from(start) << endl;
 	}
 #endif
 
 #ifdef NETWORK
-	io->set_nodelay();
 	io->sync();
-	double t1 = wallClock();
+	auto start = clock_start();
 	Integer a(128, 2, ALICE);
 	Integer b(128, 3, BOB);
 	Integer c(128, 1, PUBLIC);
 	for(int i = 0; i < 10000; ++i) {
 			cf.compute((block*)c.bits, (block*)a.bits, (block*)b.bits);
 	}
-	cout << wallClock() - t1<<" "<<party<<" "<<c.reveal<string>(BOB)<<endl;
+	cout << time_from(start)<<" "<<party<<" "<<c.reveal<string>(BOB)<<endl;
 #endif
 }
