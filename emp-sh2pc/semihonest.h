c1eea5450d46b19128f29fb2fbece92e57e3aacd
Xiao Wang <wangxiao1254@gmail.com>
2017-07-26 17:57:00 -0400

    add missing files

---
+++ emp-sh2pc/semihonest.h
@@ -0,0 +1,14 @@
+#include "emp-sh2pc/semihonest_gen.h"
+#include "emp-sh2pc/semihonest_eva.h"
+
+template<typename IO>
+static void setup_semi_honest(IO* io, int party) {
+	if(party == ALICE) {
+		local_gc = new HalfGateGen<IO>(io);
+		local_backend = new SemiHonestGen<IO>(io, (HalfGateGen<IO, RTCktOpt::on>*)local_gc);
+	} else {
+		local_gc = new HalfGateEva<IO>(io);
+		local_backend = new SemiHonestEva<IO>(io, (HalfGateEva<IO, RTCktOpt::on>*)local_gc);
+	}
+}
+
