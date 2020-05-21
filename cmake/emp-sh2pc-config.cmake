874415379ba79c44f840c3e913618b7a35e6a78e
Xiao Wang <wangxiao1254@gmail.com>
2020-05-20 21:38:35 -0500

    mionor

--- cmake/emp-sh2pc-config.cmake
+++ cmake/emp-sh2pc-config.cmake
@@ -3,7 +3,7 @@ find_package(emp-ot)
 find_path(EMP-SH2PC_INCLUDE_DIR emp-sh2pc/emp-sh2pc.h)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(EMP-SH2PC DEFAULT_MSG EMP-SH2PC_INCLUDE_DIR)
+find_package_handle_standard_args(emp-sh2pc DEFAULT_MSG EMP-SH2PC_INCLUDE_DIR)
 
 if(EMP-SH2PC_FOUND)
 	set(EMP-SH2PC_INCLUDE_DIRS ${EMP-SH2PC_INCLUDE_DIR}/include/ ${EMP-OT_INCLUDE_DIRS})
